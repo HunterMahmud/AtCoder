3303c405428b01296a065a375596274d09dc9916
Hasan Al Mahmud <128509134+HunterMahmud@users.noreply.github.com>
2023-12-22 07:09:57 +0600

    Create 2. A - Water Pressure.cpp

---
+++ 2. A - Water Pressure.cpp
@@ -0,0 +1,11 @@
+// url = https://atcoder.jp/contests/abc231/tasks/abc231_a
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  double n;
+  cin>>n;
+  cout<<(n/100.0)<<endl;
+  return 0;
+}
